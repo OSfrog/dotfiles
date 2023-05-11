bb7efa3b79f26f2c4a1736fc2f254b762f312c87
Tommy Le <tommy_8786@hotmail.com>
2023-05-11 14:31:43 +0200

    added arch dots

---
+++ fishv2/config.fish
@@ -0,0 +1,29 @@
+set fish_greeting ""
+
+set -gx TERM xterm-256color
+set -gx EDITOR nvim
+
+# theme
+set -g theme_color_scheme terminal-dark
+set -g fish_prompt_pwd_dir_length 1
+set -g theme_display_user yes
+set -g theme_hide_hostname no
+set -g theme_hostname always
+
+# aliases
+alias ls "ls -p -G"
+alias la "ls -A"
+alias ll "exa -l -g --icons"
+alias lla "ll -a"
+alias g git
+alias cat "bat"
+command -qv nvim && alias vim nvim
+
+# fzf bind Search Directory to Ctrl+F and Search Variables to Ctrl+Alt+V
+fzf_configure_bindings --directory=\cf --variables=\e\cv
+
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+  starship init fish | source
+end
