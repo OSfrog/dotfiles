f8e7eafd64459c604ac144cc5441084fe0ffce29
OSfrog <tommy_8786@hotmail.com>
2023-05-11 10:18:34 +0200

    updated lspconfig and null-ls

--- nvim/custom/configs/null-ls.lua
+++ nvim/custom/configs/null-ls.lua
@@ -1,24 +1,45 @@
-local present, null_ls = pcall(require, "null-ls")
+local null_ls = require "null-ls"
 
-if not present then
-  return
-end
-
-local b = null_ls.builtins
+local format = null_ls.builtins.formatting
+local lint = null_ls.builtins.diagnostics
+local code_actions = null_ls.builtins.code_actions
 
 local sources = {
+  -- Code actions
+  code_actions.eslint_d,
 
   -- webdev stuff
-  b.formatting.prettier,
+  format.deno_fmt,
 
   -- Lua
-  b.formatting.stylua,
+  format.stylua,
 
-  -- cpp
-  b.formatting.clang_format,
+  -- Shell
+  format.shfmt,
+
+  -- Prettier
+  format.prettier.with { filetypes = { "html", "markdown", "css", "php" } },
+
+  -- Diagnostics
+  lint.php,
+  lint.shellcheck,
 }
+
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 null_ls.setup {
   debug = true,
   sources = sources,
+  on_attach = function(client, bufnr)
+    if client.supports_method "textDocument/formatting" then
+      vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format()
+        end,
+      })
+    end
+  end,
 }
