bfab87461804f58ccdc9f78d480ea70fe103a501
Tommy Le <tle4@volvocars.com>
2024-01-17 19:24:35 +0100

    added timeout to starship and removed deno format

--- nvim/custom/configs/null-ls.lua
+++ nvim/custom/configs/null-ls.lua
@@ -1,4 +1,4 @@
-local null_ls = require "null-ls"
+local null_ls = require("null-ls")
 
 local format = null_ls.builtins.formatting
 local lint = null_ls.builtins.diagnostics
@@ -19,47 +19,47 @@ local code_actions = null_ls.builtins.code_actions
 -- }
 
 local sources = {
-  -- Code actions
-  code_actions.eslint_d,
-  -- code_actions.gitsigns,
+	-- Code actions
+	code_actions.eslint_d,
+	-- code_actions.gitsigns,
 
-  -- webdev stuff
-  format.deno_fmt,
+	-- webdev stuff
+	-- format.deno_fmt,
 
-  -- Lua
-  format.stylua,
+	-- Lua
+	format.stylua,
 
-  -- Shell
-  format.shfmt,
+	-- Shell
+	format.shfmt,
 
-  -- Fish
-  format.fish_indent,
+	-- Fish
+	format.fish_indent,
 
-  -- Prettier
-  format.prettierd,
-  -- Diagnostics
-  lint.php,
-  lint.shellcheck,
-  lint.fish,
-  -- lint.eslint_d,
-  -- lint.eslint,
+	-- Prettier
+	format.prettierd,
+	-- Diagnostics
+	lint.php,
+	lint.shellcheck,
+	lint.fish,
+	-- lint.eslint_d,
+	-- lint.eslint,
 }
 
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
-null_ls.setup {
-  debug = true,
-  sources = sources,
-  on_attach = function(client, bufnr)
-    if client.supports_method "textDocument/formatting" then
-      vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        group = augroup,
-        buffer = bufnr,
-        callback = function()
-          vim.lsp.buf.format { bufnr = bufnr }
-        end,
-      })
-    end
-  end,
-}
+null_ls.setup({
+	debug = true,
+	sources = sources,
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format({ bufnr = bufnr })
+				end,
+			})
+		end
+	end,
+})
