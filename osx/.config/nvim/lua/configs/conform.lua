17b4f1b3ad48e1a23d26b4b32f87332c0c127885
OSfrog <tommy_8786@hotmail.com>
2024-04-24 20:41:12 +0200

    change formatting opts
    Signed-off-by: OSfrog <tommy_8786@hotmail.com>

--- osx/.config/nvim/lua/configs/conform.lua
+++ osx/.config/nvim/lua/configs/conform.lua
@@ -1,31 +1,19 @@
-local slow_format_filetypes = {}
-
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
     javascript = { { "prettierd", "prettier" } },
     typescript = { { "prettierd", "prettier" } },
-    -- css = { "prettier" },
-    -- html = { "prettier" },
+    javascriptreact = { { "prettierd", "prettier" } },
+    typescriptreact = { { "prettierd", "prettier" } },
+    css = { { "prettierd", "prettier" } },
+    html = { { "prettierd", "prettier" } },
+    json = { { "prettierd", "prettier" } },
   },
 
-  format_on_save = function(bufnr)
-    -- Determine if the current file type is marked as slow
-    if slow_format_filetypes[vim.bo[bufnr].filetype] then
-      -- Optionally, perform logic for slow file types here
-      return { lsp_fallback = true }
-    else
-      local function on_format(err)
-        -- Mark the file type as slow if a timeout error occurs
-        if err and err:match "timeout$" then
-          slow_format_filetypes[vim.bo[bufnr].filetype] = true
-        end
-      end
-
-      -- Return format options for non-slow file types
-      return { timeout_ms = 200, lsp_fallback = true }, on_format
-    end
-  end,
+  format_on_save = {
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
 }
 
 require("conform").setup(options)
